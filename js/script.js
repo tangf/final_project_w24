c7d007d40e740398280707168eb4bb3eb3eefed1
tangf <tangf@umich.edu>
2024-04-21 18:12:44 -0400

    add stuff

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
+
 (function() {
     "use strict";
   
-    /**
-     * Easy selector helper function
-     */
     const select = (el, all = false) => {
       el = el.trim()
       if (all) {
@@ -13,9 +11,6 @@
       }
     }
   
-    /**
-     * Easy event listener function
-     */
     const on = (type, el, listener, all = false) => {
       let selectEl = select(el, all)
       if (selectEl) {
@@ -27,33 +22,10 @@
       }
     }
   
-    /**
-     * Easy on scroll event listener 
-     */
     const onscroll = (el, listener) => {
       el.addEventListener('scroll', listener)
     }
   
-    /**
-     * Navbar links active state on scroll
-     */
-    // let navbarlinks = select('#navbar .scrollto', true)
-    // const navbarlinksActive = () => {
-    //   console.log("hello")
-    //   let position = window.scrollY + 200
-    //   navbarlinks.forEach(navbarlink => {
-    //     if (!navbarlink.hash) return
-    //     let section = select(navbarlink.hash)
-    //     if (!section) return
-    //     if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-    //       navbarlink.classList.add('active')
-    //     } else {
-    //       navbarlink.classList.remove('active')
-    //     }
-    //   })
-    // }
-    // window.addEventListener('load', navbarlinksActive)
-    // onscroll(document, navbarlinksActive)
     let navbarlinks = select('#navbar .scrollto', true)
     const navbarlinksActive = () => {
       let position = window.scrollY + 200
@@ -73,10 +45,6 @@
     window.addEventListener('load', navbarlinksActive)
     onscroll(document, navbarlinksActive)
   
-  
-    /**
-     * Scrolls to an element with header offset
-     */
     const scrollto = (el) => {
       let elementPos = select(el).offsetTop
       window.scrollTo({
@@ -85,9 +53,8 @@
       })
     }
   
-    /**
-     * Back to top button
-     */
+    //back to top button
+
     let backtotop = select('.back-to-top')
     if (backtotop) {
       const toggleBacktotop = () => {
@@ -101,88 +68,34 @@
       onscroll(document, toggleBacktotop)
     }
   
-    /**
-     * Mobile nav toggle
-     */
+    // menu button
     on('click', '.mobile-nav-toggle', function(e) {
       select('body').classList.toggle('mobile-nav-active')
-      this.classList.toggle('bi-list')
-      this.classList.toggle('bi-x')
+      this.classList.toggle('bx-menu')
+      this.classList.toggle('bx-x-circle')
     })
   
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
     on('click', '.scrollto', function(e) {
       if (select(this.hash)) {
         e.preventDefault()
-  
+        console.log('here');
         let body = select('body')
         if (body.classList.contains('mobile-nav-active')) {
           body.classList.remove('mobile-nav-active')
           let navbarToggle = select('.mobile-nav-toggle')
-          navbarToggle.classList.toggle('bi-list')
-          navbarToggle.classList.toggle('bi-x')
+          navbarToggle.classList.toggle('bx-menu')
+          navbarToggle.classList.toggle('bx-x-circle')
         }
         scrollto(this.hash)
       }
     }, true)
-  
-    /**
-     * Scroll with ofset on page load with hash links in the url
-     */
+
     window.addEventListener('load', () => {
       if (window.location.hash) {
         if (select(window.location.hash)) {
           scrollto(window.location.hash)
         }
       }
-    });
-  
-    /**
-     * Hero type effect
-     */
-    const typed = select('.typed')
-    if (typed) {
-      let typed_strings = typed.getAttribute('data-typed-items')
-      typed_strings = typed_strings.split(',')
-      new Typed('.typed', {
-        strings: typed_strings,
-        loop: true,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 2000
-      });
-    }
-  
-    /**
-     * Skills animation
-     */
-    let skilsContent = select('.skills-content');
-    if (skilsContent) {
-      new Waypoint({
-        element: skilsContent,
-        offset: '80%',
-        handler: function(direction) {
-          let progress = select('.progress .progress-bar', true);
-          progress.forEach((el) => {
-            el.style.width = el.getAttribute('aria-valuenow') + '%'
-          });
-        }
-      })
-    }
-  
-  
-    /**
-     * Animation on scroll
-     */
-    window.addEventListener('load', () => {
-      AOS.init({
-        duration: 1000,
-        easing: 'ease-in-out',
-        once: true,
-        mirror: false
-      })
     });
   
   })()
